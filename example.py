327d39ba0e0557c45ac3bf9a23469e7a85c2b619
Gaurav <gauravnautiyal77@gmail.com>
2024-01-31 15:41:34 +0530

    again

--- example.py
+++ example.py
@@ -75,12 +75,10 @@ if __name__ == "__main__":
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
 
-
-        #For remote server Dagshub
+        predictions = lr.predict(train_x)
+        signature = infer_signature(train_x, predictions)
         remote_server_uri= "https://dagshub.com/gauravnautiyal77/mlflow_track.mlflow"
         mlflow.set_tracking_uri(remote_server_uri)
-
-      
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
         # Model registry does not work with file store
@@ -90,6 +88,7 @@ if __name__ == "__main__":
             # please refer to the doc for more information:
             # https://mlflow.org/docs/latest/model-registry.html#api-workflow
             mlflow.sklearn.log_model(
-                lr, "model", registered_model_name="ElasticnetWineModel")
+                lr, "model", registered_model_name="ElasticnetWineModel", signature=signature
+            )
         else:
-            mlflow.sklearn.log_model(lr, "model")
+            mlflow.sklearn.log_model(lr, "model", signature=signature)
